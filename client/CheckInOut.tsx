62a61e35bdd3be3ace2d7e6325ae3620ec130113
ccai1 <cathycai626@gmail.com>
2022-07-07 11:31:02 -0400

    Add new CheckInOut component

---
+++ client/CheckInOut.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+
+class CheckInOut extends React.Component<any, any> {
+    override render() {
+      return <h1>Resident Check-in/Check-out</h1>;
+    }
+  }
+
+export default CheckInOut;
