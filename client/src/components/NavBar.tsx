857d8fdd64d26b5f98bdef5fe98bca9963a956e1
Faraz Chowdhury <farazchow@gmail.com>
2022-07-22 00:04:32 -0400

    Revert "update navbar for package"
    This reverts commit 1d864255f7f01be83f9cf2f6e3d823f36d633785.
    
    Reverting

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -5,7 +5,8 @@ import "../css/navbar.css";
 const navigations = [
   { path: "/", info: "Home" },
   { path: "/profile", info: "Profile" },
-  { path: "/package", info: "Packages" },
+  { path: "/package/in", info: "Check In Package" },
+  { path: "/package/out", info: "Deliver Package" },
   { path: "/lend/items", info: "Lend Desk Items" },
   { path: "/resident/in", info: "Check In Resident" },
   { path: "/resident/out", info: "Check Out Resident" },
