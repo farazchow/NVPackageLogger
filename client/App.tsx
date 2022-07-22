dbdda67260750a9b22007608c4fc132787acf1c4
Fiona <fcai@mit.edu>
2022-07-21 21:16:37 -0400

    created initial dailynotes form UI

--- client/App.tsx
+++ client/App.tsx
@@ -10,6 +10,7 @@ import Unauthorized from "./src/pages/Unauthorizated";
 import Home from "./src/pages/Home";
 import LogPackages from "./src/views/LogPackages";
 import LendDesk from "./src/views/LendDesk";
+import DailyNotes from "./src/views/DailyNotes";
 
 // require("react-bootstrap/lib/NavbarHeader");
 
@@ -50,7 +51,7 @@ const App: FunctionComponent = () => {
 
         <Route path="/desk/workers" element={<div>Desk Workers</div>} />
         <Route path="/lost/items" element={<div>Lost Items</div>} />
-
+        <Route path="/dailynotes" element={<DailyNotes />} />
         <Route path="/login" element={<Login />} />
         <Route path="/logout" element={<div>Logout</div>} />
 
---
+++ client/src/views/DailyNotes.tsx
@@ -0,0 +1,85 @@
+import React, { FunctionComponent, useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
+
+export const DailyNotes: FunctionComponent = () => {
+  return (
+      <>
+        <h1>Daily Notes</h1>
+        <DailyNotesForm/><br></br>
+      </>
+    )
+  }
+
+
+type State = {
+  note: string,
+  deskworker: string,
+  date: string,
+  time: string,
+  isSubmitted: boolean
+};
+
+class DailyNotesForm extends React.Component<{}, State> {
+    override state = {
+      note: "",
+      deskworker: "",
+      date: "",
+      time: "",
+      isSubmitted: false
+    };
+    
+    override render() {
+        return (
+            <>
+            <br>
+            </br>
+            <div>
+            <form onSubmit={(e: React.SyntheticEvent) => {
+                e.preventDefault();
+                const target = e.target as typeof e.target & {
+                note: { value: string },
+                deskworker: { value: string },
+                date: { value: string },
+                time: { value: string }
+                };
+                this.state.note = target.note.value;
+                this.state.deskworker = target.deskworker.value;
+                this.state.date = target.date.value;
+                this.state.time = target.time.value;
+
+                console.log("submitted %s %s %s",this.state.note, this.state.deskworker, this.state.date, this.state.time);
+            }}>
+            <p>
+                <label>
+                Note: <input type="text" name="note"/>
+                </label>
+            </p>
+            
+            <p>
+                <label>
+                Deskworker: <input type="text" name="deskworker" />
+                </label>
+            </p>
+            
+            <p>
+            <label>
+                Date: <input type="text" name="date"/>
+            </label>
+            </p>
+
+            <p>
+            <label>
+                Time: <input type="text" name="time"/>
+            </label>
+            </p>
+
+            <input type="submit" value="Submit"/>
+            </form>
+            </div>
+            </>
+            
+        )
+    }
+    }
+
+export default DailyNotes;
