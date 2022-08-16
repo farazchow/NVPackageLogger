a7da7a5604e0333bd28c6bfc02ae4a4a5b60bc59
Paul Ihim <paulihim@mit.edu>
2022-08-16 06:22:19 -0400

    browser to hashrouter

--- client/index.tsx
+++ client/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as BrowserRouter } from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
